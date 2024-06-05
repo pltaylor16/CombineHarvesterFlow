50b6195a05a509bdc12e133f7d94a4c934ef6b5f
pltaylor16 <peterllewelyntaylor@gmail.com>
2024-06-05 16:55:38 -0400

    Delete CombineHarvester-flow directory

--- CombineHarvester-flow/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = """Peter Taylor"""
-__email__ = 'taylor.4264@osu.edu'
-__version__ = '1.0.0'
-
-from CombineHarvesterFlow.harvest import Harvest
-from CombineHarvesterFlow.combine import Combine
