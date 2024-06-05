67bb7a0d474ec2dbe1e6b8944acc6fe43c84a501
pltaylor16 <peterllewelyntaylor@gmail.com>
2024-06-05 16:36:44 -0400

    renaming directories to match new name

---
+++ CombineHarvester-flow/__init__.py
@@ -0,0 +1,6 @@
+__author__ = """Peter Taylor"""
+__email__ = 'taylor.4264@osu.edu'
+__version__ = '1.0.0'
+
+from CombineHarvester-flow.harvest import Harvest
+from CombineHarvester-flow.combine import Combine
