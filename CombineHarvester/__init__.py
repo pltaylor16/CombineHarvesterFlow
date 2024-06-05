d690a9f2469ce07a49f0cd9e4aacd41d11eb37b5
Andrei Cuceu <andreicuceu@gmail.com>
2024-06-05 15:32:37 -0400

    Update to version 1.0.0

--- CombineHarvester/__init__.py
+++ CombineHarvester/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Peter Taylor"""
 __email__ = 'taylor.4264@osu.edu'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 
 from CombineHarvester.harvest import Harvest
 from CombineHarvester.combine import Combine
