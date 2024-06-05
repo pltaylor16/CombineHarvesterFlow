5c691dd774ca055d65e6fb980b370ecc229f8217
pltaylor16 <peterllewelyntaylor@gmail.com>
2024-06-05 16:37:31 -0400

    Delete old CombineHarvester directory

--- CombineHarvester/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = """Peter Taylor"""
-__email__ = 'taylor.4264@osu.edu'
-__version__ = '1.0.0'
-
-from CombineHarvester-flow.harvest import Harvest
-from CombineHarvester-flow.combine import Combine
